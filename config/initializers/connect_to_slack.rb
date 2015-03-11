ade149b1a25e434192f6077a54ab14a073e93a09
Bob Lail <bob.lail@cph.org>
2015-03-11 15:48:31 -0500

    [skip] Skipped connecting to Slack when Houston isn't begin run as a server (5m)

--- config/initializers/connect_to_slack.rb
+++ config/initializers/connect_to_slack.rb
@@ -1,1 +1,6 @@
-Houston::Slack.connection.listen!
+if Houston.server?
+  Houston::Slack.connection.listen!
+else
+  puts "\e[94mSkipping Slack Listener since we're not running as a server\e[0m"
+  Rails.logger.info "\e[94mSkipping Slack Listener since we're not running as a server\e[0m"
+end
