e79748628b89810f8784ee14aafb327f690807dd
Bob Lail <bob.lail@cph.org>
2015-09-08 19:45:07 -0500

    [refactor] Refactored config (1m)

--- lib/houston/slack.rb
+++ lib/houston/slack.rb
@@ -5,16 +5,20 @@ require "houston/slack/connection"
 module Houston
   module Slack
     extend self
-    
-    attr_reader :config
+
     attr_reader :connection
-    
+
     def send(message, options)
       connection.send_message(message, options)
     end
-    
+
+    def config(&block)
+      @configuration ||= Slack::Configuration.new
+      @configuration.instance_eval(&block) if block_given?
+      @configuration
+    end
+
   end
-  
-  Slack.instance_variable_set :@config, Slack::Configuration.new
+
   Slack.instance_variable_set :@connection, Slack::Connection.new
 end
--- lib/houston/slack/configuration.rb
+++ lib/houston/slack/configuration.rb
@@ -4,37 +4,35 @@ require "thread_safe"
 module Houston::Slack
   class Configuration
     attr_reader :listeners
-    
+
     def initialize
       @listeners = ThreadSafe::Array.new
       @typing_speed = 100.0
-      config = Houston.config.module(:slack).config
-      instance_eval(&config) if config
     end
-    
+
     # Define configuration DSL here
-    
+
     def token(*args)
       @token = args.first if args.any?
       @token
     end
-    
+
     def typing_speed(*args)
       @typing_speed = args.first.to_f if args.any?
       @typing_speed
     end
-    
+
     def listen_for(matcher, &block)
       Listener.new(matcher, true, block).tap do |listener|
         @listeners.push listener
       end
     end
-    
+
     def overhear(matcher, &block)
       Listener.new(matcher, false, block).tap do |listener|
         @listeners.push listener
       end
     end
-    
+
   end
 end
