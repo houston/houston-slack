797f06974a115c2f5d1c68c1372c174c172d0b17
Bob Lail <bob.lail@cph.org>
2015-10-10 11:33:33 -0500

    [skip] Namespaced routes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Houston::Slack::Engine.routes.draw do
 
-  get "/", to: "slack#show"
-  post "/connect", to: "slack#connect"
+  scope "slack" do
+    get "/", to: "slack#show"
+    post "/connect", to: "slack#connect"
+  end
 
 end
