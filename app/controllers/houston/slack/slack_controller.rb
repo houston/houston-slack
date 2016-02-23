13e3a373ef614f9648c7f2472ee5ef1509128998
Bob Lail <bob.lailfamily@gmail.com>
2016-02-22 19:00:36 -0600

    [skip] Added the suggested route for Slack to verify Houston's SSL certificate (1m)

--- app/controllers/houston/slack/slack_controller.rb
+++ app/controllers/houston/slack/slack_controller.rb
@@ -13,6 +13,15 @@ module Houston::Slack
       redirect_to action: :show
     end
 
+    # Before submitting a command to your server, Slack will occasionally send
+    # your command URLs a simple GET request to verify the certificate. These
+    # requests will include a parameter ssl_check set to 1. Mostly, you may
+    # ignore these requests, but please do respond with a HTTP 200 OK.
+    # https://api.slack.com/slash-commands#best_practices
+    def ssl_verify
+      head :ok
+    end
+
     def command
       command_name = params.fetch(:command).gsub(/^\//, "")
       command = Houston::Slack.config.slash_commands[command_name]
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Houston::Slack::Engine.routes.draw do
   scope "slack" do
     get "/", to: "slack#show"
     post "/connect", to: "slack#connect"
+    get "/command", to: "slack#ssl_verify"
     post "/command", to: "slack#command"
   end
 
