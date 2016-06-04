665ea4d5853a6beda9b6f06db7fa668b1e492761
Bob Lail <bob.lailfamily@gmail.com>
2016-06-04 11:54:16 -0500

    [skip] Used Houston.async (1m)

--- lib/houston/slack/session.rb
+++ lib/houston/slack/session.rb
@@ -87,15 +87,13 @@ module Houston
       def invoke!(listener, e)
         Rails.logger.debug "\e[35m[slack:hear:#{e.message.type}] #{e.message}\e[0m"
 
-        Thread.new do
+        Houston.async do
           begin
             listener.call(e)
           rescue Exception # rescues StandardError by default; but we want to rescue and report all errors
             Houston.report_exception $!, parameters: {channel: e.channel, message: e.message, sender: e.sender}
             Rails.logger.error "\e[31m[slack:exception] (#{$!.class}) #{$!.message}\n  #{$!.backtrace.join("\n  ")}\e[0m"
             e.reply "An error occurred when I was trying to answer you"
-          ensure
-            ActiveRecord::Base.clear_active_connections!
           end
         end
       end
