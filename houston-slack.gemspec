89dcc6d64a70c40d2b480d8b49744c2ef77e1487
Bob Lail <bob.lailfamily@gmail.com>
2016-06-25 18:22:10 -0500

    [skip] Upgraded to houston-core 0.7.0.beta (4m)

--- houston-slack.gemspec
+++ houston-slack.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "concurrent-ruby"
   spec.add_dependency "slacks"
 
-  spec.add_development_dependency "bundler", "~> 1.10.6"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "houston-core", ">= 0.5.3"
+  spec.add_development_dependency "bundler", "~> 1.11.2"
+  spec.add_development_dependency "rake", "~> 11.2"
+  spec.add_development_dependency "houston-core", ">= 0.7.0.beta"
 end
--- lib/houston/slack/user_ext.rb
+++ lib/houston/slack/user_ext.rb
@@ -10,41 +10,26 @@ module Houston
           username = username.username if username.is_a? Slacks::User
           username = username.gsub(/^@/, "")
 
-          # If we can already identify the user who has the given username, return them
-          user = ::User.where(["view_options->'slack_username' = ?", username]).first
-          return user if user
+          find_by_prop "slack.username", username do |username|
 
-          # Look up the email address of the Slack user and see if we can
-          # identify the Houston user by the Slack user's email address.
-          user = Houston::Slack.connection.users.detect { |user| user["name"] == username }
-          user = find_by_email_address user["profile"]["email"] if user
-          return nil unless user
+            # Look up the email address of the Slack user and see if we can
+            # identify the Houston user by the Slack user's email address.
+            user = Houston::Slack.connection.users.detect { |user| user["name"] == username }
+            user = find_by_email_address user["profile"]["email"] if user
 
-          # If we have identified the user, store their username so that
-          # we can skip the email-lookup step in the future.
-          user.set_slack_username! username
-          user
+          end
         end
       end
 
       def slack_username
-        # If we've previously identified this user as a Slack user,
-        # make sure that their username is still valid. If so, return it.
-        username = view_options["slack_username"]
-        username = "@" + username if username
-        return username if Houston::Slack.connection.user_exists?(username)
+        username = get_prop "slack.username" do
 
-        # Look for a Slack user that has one of this Houston user's
-        # email addresses.
-        user = Houston::Slack.connection.users.detect { |user|
-          email_addresses.member?(user["profile"]["email"]) }
-        return nil unless user
+          # Look for a Slack user that has one of this Houston user's email addresses.
+          user = Houston::Slack.connection.users.detect { |user| email_addresses.member?(user["profile"]["email"]) }
+          user && user["name"]
 
-        # If we have identified the user, store their username so that
-        # we can skip the email-lookup step in the future.
-        username = "@" + user["name"]
-        set_slack_username! username
-        username
+        end
+        "@" + username
       end
       alias :slack_channel :slack_username
 
