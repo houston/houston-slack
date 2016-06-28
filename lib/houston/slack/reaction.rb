7c0736320a21508dd453fcdef59896744efc0a71
Bob Lail <bob.lailfamily@gmail.com>
2016-06-27 19:08:47 -0500

    [skip] Gave better information when failing to instantiate a Reaction (10m)

--- lib/houston/slack/reaction.rb
+++ lib/houston/slack/reaction.rb
@@ -6,14 +6,20 @@ module Houston
       attr_reader :emoji
 
       def initialize(session, data)
-        message = Houston::Slack.connection.get_message data["item"]["channel"], data["item"]["ts"]
-        raise "Unable to fetch message" unless message["ok"]
-        sender = session.slack.find_user data["user"]
-        message = Houston::Slack::Message.new(session,
-          message.slice("type", "channel").merge(
-            message["message"].slice("user", "text", "ts", "attachments", "reactions")))
-        super(session: session, message: message, channel: message.channel, sender: sender)
         @emoji = data["reaction"]
+        sender = session.slack.find_user data["user"]
+
+        message = Houston::Slack.connection.get_message data["item"]["channel"], data["item"]["ts"]
+        raise Slacks::ResponseError.new(message, message["error"]) unless message["ok"]
+        message_data = message.slice("type", "channel").merge(
+          message["message"].slice("user", "text", "ts", "attachments", "reactions"))
+        message = Houston::Slack::Message.new(session, message_data)
+
+        super(session: session, message: message, channel: message.channel, sender: sender)
+      rescue
+        $!.additional_information["data"] = data
+        $!.additional_information["message"] = message_data
+        raise
       end
 
     end
